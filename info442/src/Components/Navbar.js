275fad0bc849941563f27f287bbcdfe44a26b4e5
Ayykody <kody.chanthavong@gmail.com>
2024-01-31 14:52:35 -0800

    test

---
+++ info442/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav className=''> 
+        <a href='#' className=''>
+            <img src='' alt=''/>
+        </a>
+    </nav>
+  )
+}
+
+export default NavBar;
