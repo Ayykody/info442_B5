ca54940db206a4a95ca2d55d5e36ede92e762d7c
Ayykody <kody.chanthavong@gmail.com>
2024-01-31 15:07:14 -0800

    will finish rest of navbar and header later

--- info442/src/Components/Navbar.js
+++ info442/src/Components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../Images/Fish.png';
 
 function NavBar() {
   return (
     <nav className=''> 
         <a href='#' className=''>
-            <img src='' alt=''/>
+            <img src={logo} alt=''/>
         </a>
     </nav>
   )
