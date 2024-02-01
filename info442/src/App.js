fb099de64acc58102d1e268700e3d9ab67e24747
Ayykody <kody.chanthavong@gmail.com>
2024-02-01 14:16:01 -0800

    just need to fix image border

--- info442/src/App.js
+++ info442/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { NavBar } from './Components/NavBar'
+import { NavBar } from './Components/Navbar'
 import Resources from './Components/Resources'
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom"
+import Quiz from './Components/Quiz'
+import Events from './Components/Events'
 
 export default function App(props) {
   return (
@@ -9,7 +11,9 @@ export default function App(props) {
       <Router>
       <NavBar />
         <Routes>
+          <Route path="/Quiz" element={<Quiz />} />
           <Route path="/Resources" element={<Resources />} />
+          <Route path="/Events" element={<Events />} />
         </Routes>
       </Router>
     </div>
--- info442/src/Components/Navbar.js
+++ info442/src/Components/Navbar.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from '../Images/Fish.png';
 import { Link } from 'react-router-dom';
+import "./Navbar.css"
 
 export const NavBar = ()  => {
+
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () => {
+    if(window.scrolly >= 50){
+      setnav(true);
+    }
+    else{
+      setnav(false);
+    }
+  }
+  window.addEventListener('scroll', changeBackground)
   return (
-    <nav className=''> 
-        <a href='#' className=''>
-            <img src={logo} alt=''/>
+    <nav className={nav ? 'nav active': 'nav'}> 
+        <a href='#' className='logo'>
+            <img src={logo} alt='fish'/>
         </a>
         <ul>
           <li>
+            <Link to='/Quiz'>Quiz</Link>
             <Link to='/Resources'>Resources</Link>
+            <Link to='/Events'>Events</Link>
           </li>
         </ul>
     </nav>
