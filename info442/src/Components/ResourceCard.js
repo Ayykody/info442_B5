25dd90299ca6e027ed6cdd46050ae871d50ce003
kassidyg-2066372 <kassidyg@uw.edu>
2024-02-08 01:08:37 -0800

    Added images to resource cards

--- info442/src/Components/ResourceCard.js
+++ info442/src/Components/ResourceCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './ResourceCard.css'
 
-function ResourceCard({ title, description }) {
+function ResourceCard({ title, description, imageLocation }) {
   return (
     <div className="resource-card">
-      <h2>{title}</h2>
-      <p>{description}</p>
+        <img src={imageLocation} alt={title} style={{ width: '200px', height: '150px' }} />
+        <h2>{title}</h2>
+        <p>{description}</p>
     </div>
   );
 }
--- info442/src/Components/Resources.js
+++ info442/src/Components/Resources.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ResourceCard from './ResourceCard';
+import trash from '../Images/trash.jpg';
+import overfishing from '../Images/overfishing.jpeg';
+import noise from '../Images/noise_pollution.png';
+import reefs from '../Images/reefs.jpeg';
+import footprint from '../Images/carbon_footprint.jpeg';
+import sunscreen from '../Images/sunscreen.png';
 
 function Resources() {
   return (
@@ -7,26 +13,32 @@ function Resources() {
       <ResourceCard
         title="Trash Left Behind by Tourists"
         description="Description."
+        imageLocation={trash}
       />
       <ResourceCard
         title="Overfishing in Tourist-Heavy Areas"
         description="Description."
+        imageLocation={overfishing}
       />
       <ResourceCard
         title="Noise Pollution from Tour Boats"
         description="Description."
+        imageLocation={noise}
       />
       <ResourceCard
         title="Degradation of Coral Reefs"
         description="Description."
+        imageLocation={reefs}
       />
       <ResourceCard
         title="Carbon Footprint of Marine Tourism"
         description="Description."
+        imageLocation={footprint}
       />
       <ResourceCard
         title="Toxicity of Sunscreen"
         description="Description."
+        imageLocation={sunscreen}
       />
     </div>
   );
