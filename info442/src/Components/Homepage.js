0349ffc7c595fa18321558348ed0fe0d98d6e5db
Ayykody <kody.chanthavong@gmail.com>
2024-02-12 23:46:41 -0800

    added divs to homepage and some css for all the pages

--- info442/src/Components/Homepage.js
+++ info442/src/Components/Homepage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Homepage.css';
+import { Link } from 'react-router-dom';
 
 function Homepage() {
   return (
-    <div className="container">
-      <h1>Let's explore tourism and its connection to the ocean</h1>
-      <p>Did you know?</p>
-   </div>
+    <body className="body">
+      <div className="container">
+        <h1>Ocean Biodversity</h1>
+        <p>Do you know the risk of traveling?</p>
+    </div>
+    <div>
+      <h1 className="container">Take the Quiz!</h1>
+      <div className="b">
+        <Link to="/Quiz">
+          <button>Go to Quiz</button>
+        </Link>
+      </div>
+    </div>
+    <div>
+      /* Add calculator here */
+    </div>
+   </body>
   )
 }
 
