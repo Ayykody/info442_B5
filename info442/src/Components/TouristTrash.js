82ea4d4da3b9b0540437c359fc9c60de8d860006
kassidyg-2066372 <kassidyg@uw.edu>
2024-02-20 18:05:30 -0800

    add more info to resource page

--- info442/src/Components/TouristTrash.js
+++ info442/src/Components/TouristTrash.js
@@ -13,6 +13,19 @@ function TouristTrash() {
             locals living in coastal communities that depend on tourism for their economy. Learn about the impacts of trash left by tourists and what you 
             can do to reduce your impact when you travel.
             </p>
+            <div className='tips-card'>
+                <h2>General Tips to Reduce your Trash while Traveling</h2>
+                <ol>
+                    <li>Reduce the use of single-use plastics</li>
+                        <ul>
+                            <li>You may be exploring new foods and eating out a lot, which may mean more plastic silverware. Bring some reusable
+                                silverware and wash them at the place you are staying.
+                            </li>
+                            <li>Bring reusable bags for your trip to use while you are shopping in the coastal communities.</li>
+                        </ul>
+                    <li>Research local recycling practices before you leave for your trip</li>
+                </ol>
+            </div>
         </div>
     )   
 }
